f69ae382b37f198bd4f3735be85a6f66c469155e
Daniel-at-github <dsanfab+github@gmail.com>
2018-07-27 16:28:10 +0200

    Initial commit

---
+++ pyvideo_scrape.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"Scrape several conferences into pyvideo repository"
+
+
+def main():
+    "Scrape several conferences into pyvideo repository"
+    pass
+
+
+if __name__ == '__main__':
+    main()
