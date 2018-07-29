8093000eb01537e0c0d0f6c21e51c7549b78371f
Daniel-at-github <dsanfab+github@gmail.com>
2018-07-29 14:42:28 +0200

    Parse event data

--- pyvideo_scrape.py
+++ pyvideo_scrape.py
@@ -11,7 +11,7 @@ import yaml
 LOGGER = None
 
 
-def setup_logger():
+def setup_logger() -> logging.Logger:
     """Return a logger with a default ColoredFormatter."""
     # From https://github.com/borntyping/python-colorlog/blob/master/doc/
     #   example.py
@@ -43,6 +43,37 @@ def load_events(fich):
         return conf
 
 
+class Event:
+    """PyVideo Event metadata"""
+
+    def __init__(self, event_data: dict, repo_dir):
+        self.repository = repo_dir
+        self.title = event_data['title']
+        for mandatory_field in ['title', 'dir', 'issue', 'youtube_list']:
+            if mandatory_field in event_data and event_data[mandatory_field]:
+                pass
+            else:
+                LOGGER.error('No %s data in conference %s', mandatory_field, self.title)
+                raise ValueError("{} can't be null".format(mandatory_field))
+        self.directory = event_data['dir']
+        self.issue = event_data['issue']
+        if isinstance(event_data['youtube_list'], str):
+            self.youtube_lists = [event_data['youtube_list']]
+        elif isinstance(event_data['youtube_list'], list):
+            self.youtube_lists = event_data['youtube_list']
+        else:
+            raise TypeError("youtube_list must be a string or a list of strings")
+        self.related_urls = event_data.get('related_urls', {})
+        self.language = event_data.get('language', None)
+        self.tags = event_data.get('tags', [])
+        if 'dates' in event_data and event_data['dates']:
+            self.know_date = True
+            self.date_begin = str(event_data['dates']['begin'])
+            self.date_end = str(event_data['dates'].get('end', self.date_begin))
+            self.date_default = str(event_data['dates'].get('default', self.date_begin))
+        else:
+            self.know_date = False
+
 def main():
     """Scrape several conferences into pyvideo repository"""
 
@@ -55,6 +86,8 @@ def main():
     events_file = 'events.yml'
     events_data = load_events(events_file)
 
+    pyvideo_repo = pathlib.PosixPath(events_data['repo_dir']).expanduser().resolve()
+    events = [Event(event_data, repo_dir=pyvideo_repo) for event_data in events_data['events']]
     time_end = datetime.datetime.now()
     time_delta = str(time_end - time_init)
     LOGGER.debug('Time init: %s', time_init)
